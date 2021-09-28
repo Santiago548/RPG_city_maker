91a2b21916335e18fce4d2695a1e8ac0b2299148
Dominic Barajas <dominicbarajas548@gmail.com>
2021-09-27 18:54:57 -0600

    backend rails
    create backend with rails for generator

---
+++ frontend/src/containers/cityRandom.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class cityRandom extends Component {
+
+    handleClick = event => {
+        const population 
+        const region
+        const government
+        const importMaterials
+        const exportMaterials
+        const armorer
+        const weaponSmith
+        const supplies
+        const race1
+        const race2
+        const race3
+        const religion1
+        const religion2
+        const religion3
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
