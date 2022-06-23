1a14239ea34b227ad4cc09ebb583aa6e128cd96f
Santiago548 <dominicbarajas548@gmail.com>
2022-06-22 19:53:19 -0600

    add new actions for delete and other functions

--- backend/db/migrate/20210928162057_create_cities.rb
+++ backend/db/migrate/20210928162057_create_cities.rb
@@ -1,6 +1,7 @@
 class CreateCities < ActiveRecord::Migration[6.1]
   def change
     create_table :cities do |t|
+      t.string :name
       t.integer :population
       t.string :environment
       t.string :government
